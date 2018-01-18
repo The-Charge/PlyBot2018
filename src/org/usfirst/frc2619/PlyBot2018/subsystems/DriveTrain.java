142384230f0a7515de9c9b0f411ec299013055b7
RALah <alexanderlehmann2001@gmail.com>
2018-01-18 01:55:51 +0100

    Revert "Revert "Added invertRun(double, double, boolean) Method, this Method multiplies rightSpeed and leftSpeed *=-1 if the boolean is true;""
    This reverts commit cb595edf239ceffdfbdd081f627c695ea6ed6f22.

--- src/org/usfirst/frc2619/PlyBot2018/subsystems/DriveTrain.java
+++ src/org/usfirst/frc2619/PlyBot2018/subsystems/DriveTrain.java
@@ -61,8 +61,18 @@ public class DriveTrain extends Subsystem {
     }
 
     public void run(double leftSpeed, double rightSpeed) {
-    	leftFrontMotor.set(leftSpeed);
-    	rightFrontMotor.set(-rightSpeed);
+    	invertRun(leftSpeed, rightSpeed, false);
+    }
+    
+    public void invertRun(double leftSpeed, double rightSpeed, boolean invert) {
+    	if (invert) {
+    		leftSpeed*=-1;
+    		rightSpeed*=-1;
+    	}
+    	
+	    leftFrontMotor.set(leftSpeed);
+	    rightFrontMotor.set(-rightSpeed);
+
     	SmartDashboard.putNumber("Encoder", pot.get());
     	SmartDashboard.putNumber("Encoder Position Left", leftFrontMotor.getSelectedSensorPosition(0));
     	SmartDashboard.putNumber("Encoder Position Right", rightFrontMotor.getSelectedSensorPosition(0));
