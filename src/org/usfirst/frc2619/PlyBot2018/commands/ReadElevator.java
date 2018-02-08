bfc234111e8fd4744e3d5e9dd98f0039b82ec291
Sandy-Laptop\abcurrie <abcurrie@gmail.com>
2018-02-07 22:18:46 -0500

    Fixed string case problem with TestElevator

--- src/org/usfirst/frc2619/PlyBot2018/commands/ReadElevator.java
+++ src/org/usfirst/frc2619/PlyBot2018/commands/ReadElevator.java
@@ -12,7 +12,6 @@
 package org.usfirst.frc2619.PlyBot2018.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc2619.PlyBot2018.Robot;
-import org.usfirst.frc2619.PlyBot2018.subsystems.testElevator;
 
 /**
  *
