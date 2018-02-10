e62eef93c1535daa30b11b360db4f4a41d57d695
David Wang <davidrocks25408@gmail.com>
2018-02-10 15:48:24 -0500

    Counter is not showing up in SmartDashboard. Put count() in the execute() of ElevateToX(). Needs testing.

--- src/org/usfirst/frc2619/PlyBot2018/commands/ElevateToXFeet.java
+++ src/org/usfirst/frc2619/PlyBot2018/commands/ElevateToXFeet.java
@@ -45,6 +45,7 @@ public class ElevateToXFeet extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+    	Robot.elevator.count();
     }
 
     // Make this return true when this Command no longer needs to run execute()
